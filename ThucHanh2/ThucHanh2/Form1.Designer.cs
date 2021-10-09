0e8616383ff30218b3760e1cf47a22c3e8d99e49
xuanduz <xuanduc942001@gmail.com>
2021-10-09 07:48:21 +0700

    thuc hanh 2

---
+++ ThucHanh2/ThucHanh2/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿
+namespace ThucHanh2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnThemDS = new System.Windows.Forms.Button();
+            this.txtDiem = new System.Windows.Forms.TextBox();
+            this.txtTinChi = new System.Windows.Forms.TextBox();
+            this.cbTenMon = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lstDS = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtTongTC = new System.Windows.Forms.TextBox();
+            this.txtTongDiem = new System.Windows.Forms.TextBox();
+            this.txtDiemTB = new System.Windows.Forms.TextBox();
+            this.btnTinh = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnThemDS);
+            this.groupBox1.Controls.Add(this.txtDiem);
+            this.groupBox1.Controls.Add(this.txtTinChi);
+            this.groupBox1.Controls.Add(this.cbTenMon);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(19, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(262, 396);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin điểm sinh viên";
+            // 
+            // btnThemDS
+            // 
+            this.btnThemDS.Location = new System.Drawing.Point(51, 298);
+            this.btnThemDS.Name = "btnThemDS";
+            this.btnThemDS.Size = new System.Drawing.Size(143, 34);
+            this.btnThemDS.TabIndex = 6;
+            this.btnThemDS.Text = "Thêm vào &DS";
+            this.btnThemDS.UseVisualStyleBackColor = true;
+            this.btnThemDS.Click += new System.EventHandler(this.btnThemDS_Click);
+            // 
+            // txtDiem
+            // 
+            this.txtDiem.Location = new System.Drawing.Point(34, 220);
+            this.txtDiem.Name = "txtDiem";
+            this.txtDiem.Size = new System.Drawing.Size(181, 23);
+            this.txtDiem.TabIndex = 5;
+            // 
+            // txtTinChi
+            // 
+            this.txtTinChi.Location = new System.Drawing.Point(34, 141);
+            this.txtTinChi.Name = "txtTinChi";
+            this.txtTinChi.ReadOnly = true;
+            this.txtTinChi.Size = new System.Drawing.Size(181, 23);
+            this.txtTinChi.TabIndex = 4;
+            // 
+            // cbTenMon
+            // 
+            this.cbTenMon.FormattingEnabled = true;
+            this.cbTenMon.Items.AddRange(new object[] {
+            "Tin học đại cương",
+            "Giải tích F1",
+            "Tiếng Anh A0",
+            "Triết học Mác-Lênin",
+            "Vật lý F1"});
+            this.cbTenMon.Location = new System.Drawing.Point(34, 72);
+            this.cbTenMon.Name = "cbTenMon";
+            this.cbTenMon.Size = new System.Drawing.Size(181, 23);
+            this.cbTenMon.TabIndex = 3;
+            this.cbTenMon.SelectedIndexChanged += new System.EventHandler(this.cbTenMon_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 184);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Điểm";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Số tín chỉ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên môn học";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lstDS);
+            this.groupBox2.Location = new System.Drawing.Point(287, 27);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(491, 159);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Danh sách các môn học";
+            // 
+            // lstDS
+            // 
+            this.lstDS.FormattingEnabled = true;
+            this.lstDS.ItemHeight = 15;
+            this.lstDS.Location = new System.Drawing.Point(12, 38);
+            this.lstDS.Name = "lstDS";
+            this.lstDS.Size = new System.Drawing.Size(464, 94);
+            this.lstDS.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(299, 216);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 15);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Tổng số tín chỉ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(299, 273);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 15);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Điểm trung bình";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(547, 216);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 15);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Tổng số điểm";
+            // 
+            // txtTongTC
+            // 
+            this.txtTongTC.Location = new System.Drawing.Point(402, 208);
+            this.txtTongTC.Name = "txtTongTC";
+            this.txtTongTC.Size = new System.Drawing.Size(98, 23);
+            this.txtTongTC.TabIndex = 6;
+            // 
+            // txtTongDiem
+            // 
+            this.txtTongDiem.Location = new System.Drawing.Point(645, 206);
+            this.txtTongDiem.Name = "txtTongDiem";
+            this.txtTongDiem.Size = new System.Drawing.Size(98, 23);
+            this.txtTongDiem.TabIndex = 7;
+            // 
+            // txtDiemTB
+            // 
+            this.txtDiemTB.Location = new System.Drawing.Point(419, 265);
+            this.txtDiemTB.Name = "txtDiemTB";
+            this.txtDiemTB.Size = new System.Drawing.Size(145, 23);
+            this.txtDiemTB.TabIndex = 8;
+            // 
+            // btnTinh
+            // 
+            this.btnTinh.Location = new System.Drawing.Point(359, 358);
+            this.btnTinh.Name = "btnTinh";
+            this.btnTinh.Size = new System.Drawing.Size(112, 34);
+            this.btnTinh.TabIndex = 7;
+            this.btnTinh.Text = "&Tính";
+            this.btnTinh.UseVisualStyleBackColor = true;
+            this.btnTinh.Click += new System.EventHandler(this.btnTinh_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(570, 358);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(109, 34);
+            this.btnThoat.TabIndex = 8;
+            this.btnThoat.Text = "T&hoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnTinh);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.txtDiemTB);
+            this.Controls.Add(this.txtTongDiem);
+            this.Controls.Add(this.txtTongTC);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cbTenMon;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnThemDS;
+        private System.Windows.Forms.TextBox txtDiem;
+        private System.Windows.Forms.TextBox txtTinChi;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtTongTC;
+        private System.Windows.Forms.TextBox txtTongDiem;
+        private System.Windows.Forms.TextBox txtDiemTB;
+        private System.Windows.Forms.Button btnTinh;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.ListBox lstDS;
+    }
+}
+
---
+++ ThucHanh2/ThucHanh2/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThucHanh2
+{
+    public partial class Form1 : Form
+    {
+        int TongTC = 0;
+        double TongDiem = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cbTenMon_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (cbTenMon.SelectedIndex)
+            {
+                case 0 :
+                    txtTinChi.Text = "2"; 
+                    break;
+                case 1:
+                    txtTinChi.Text = "3";
+                    break;
+                case 2:
+                    txtTinChi.Text = "3";
+                    break;
+                case 3:
+                    txtTinChi.Text = "2";
+                    break;
+                case 4:
+                    txtTinChi.Text = "3";
+                    break;
+                default:
+                    break;
+
+            }
+
+        }
+
+        private void btnThemDS_Click(object sender, EventArgs e)
+        {
+            if(txtDiem.Text != "")
+            {
+                double d;
+                bool isNumeric = double.TryParse(txtDiem.Text, out d);
+                if (isNumeric)
+                {
+                    if(d <= 10 && d >= 0)
+                    {
+                        lstDS.Items.Add(cbTenMon.SelectedItem.ToString() + "    " + txtTinChi.Text + "-" + txtDiem.Text);
+                        TongTC += int.Parse(txtTinChi.Text);
+                        txtTongTC.Text = TongTC.ToString();
+
+                        TongDiem += int.Parse(txtTinChi.Text) * double.Parse(txtDiem.Text);
+                        txtTongDiem.Text = TongDiem.ToString();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Điểm phải trong khoảng [0, 10]");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Bạn phải nhập sai điểm");
+                }  
+            }
+            else
+            {
+                MessageBox.Show("Bạn chưa nhập điểm");
+            }
+            
+        }
+
+        private void btnTinh_Click(object sender, EventArgs e)
+        {
+            txtDiemTB.Text = (double.Parse(txtTongDiem.Text) / double.Parse(txtTongTC.Text)).ToString();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Bạn muốn thoát ?", "Exit", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            
+        }
+    }
+}
